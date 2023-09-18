adf334607bf672bf90425c89201902526d34dfae
Eva-Wamuyu <rutheve.eva@gmail.com>
2023-09-18 13:22:59 +0300

    .

--- Config/db.config.js
+++ Config/db.config.js
@@ -13,7 +13,7 @@ export const sqlConfig = {
     },
     options: {
         encrypt: true, // for azure
-        trustServerCertificate: true // change to true for local dev / self-signed certs
+        trustServerCertificate: false // change to true for local dev / self-signed certs
     }
 }
 
