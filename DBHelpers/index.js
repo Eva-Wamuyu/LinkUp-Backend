8c4409a49e8e4571ad7968e18869a31ae9c1fcff
Wamuyu <rutheve.eva@gmail.com>
2026-02-06 07:17:34 +0300

    conn pool

--- DBHelpers/index.js
+++ DBHelpers/index.js
@@ -3,19 +3,32 @@ import { sqlConfig } from "../Config/db.config.js";
 import mssql from 'mssql'
 
 export class DB {
+    static pool;
+
+    static async getPool() {
+        if (!this.pool) {
+            this.pool = await mssql.connect(sqlConfig);
+        }
+        return this.pool;
+    }
 
     static async exec(storedProcedure, data = {}) {
         try {
-            const pool = mssql.connect(sqlConfig);
-            const request = this.addData(pool.request(), data);
-            return await request.execute(storedProcedure);
+            const pool = await this.getPool();
+            let request = pool.request();
+
+            console.log(`[DB] Connected successfully. Executing stored procedure: ${storedProcedure}`);
+            request = this.addData(request, data);
+            const result = await request.execute(storedProcedure);
+            console.log(`[DB] Stored procedure ${storedProcedure} executed successfully`);
+            return result;
         } catch (error) {
-            console.log("DB.EXEC Stored procedure failed", {
-                storedProcedure,
-                message: error.message,
-                stack: error.stack
-            })
-            throw error;
+            console.error(`[DB] Error executing ${storedProcedure}:`, error.message);
+            console.error(`[DB] Error details:`, {
+                code: error.code,
+                state: error.state,
+            });
+            throw error
         }
     }
 
